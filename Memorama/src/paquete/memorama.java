39ade1866cf84b738f8c55879c00c358f794e3da
Manuel-O-12 <joor_23@alu.uabcs.mx>
2025-03-20 00:26:24 -0700

    unidad 3 ejercicio 9

--- Memorama/src/paquete/memorama.java
+++ Memorama/src/paquete/memorama.java
@@ -6,6 +6,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 
 public class memorama {
 
@@ -46,9 +52,96 @@ public class memorama {
 		panel.setForeground(new Color(0, 0, 0));
 		frame.getContentPane().add(panel, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel = new JLabel("MEMORAMA");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		panel.add(lblNewLabel);
+		
 		JPanel panel_1 = new JPanel();
 		panel_1.setForeground(new Color(128, 255, 255));
 		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
+		
+		JPanel panel_2 = new JPanel();
+		panel_1.add(panel_2);
+		panel_2.setLayout(new GridLayout(4, 4, 0, 0));
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setIcon(new ImageIcon(memorama.class.getResource("/paquete/carrito1.jpg")));
+		panel_2.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("");
+		btnNewButton_1.setIcon(new ImageIcon(memorama.class.getResource("/paquete/carrito1.jpg")));
+		panel_2.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("");
+		btnNewButton_2.setIcon(new ImageIcon(memorama.class.getResource("/paquete/luna1.jpg")));
+		panel_2.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("");
+		btnNewButton_3.setIcon(new ImageIcon(memorama.class.getResource("/paquete/luna1.jpg")));
+		panel_2.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("");
+		btnNewButton_4.setIcon(new ImageIcon(memorama.class.getResource("/paquete/nave1.jpg")));
+		panel_2.add(btnNewButton_4);
+		
+		JButton btnNewButton_12 = new JButton("");
+		btnNewButton_12.setIcon(new ImageIcon(memorama.class.getResource("/paquete/nave1.jpg")));
+		panel_2.add(btnNewButton_12);
+		
+		JButton btnNewButton_5 = new JButton("");
+		btnNewButton_5.setIcon(new ImageIcon(memorama.class.getResource("/paquete/ovni1.jpg")));
+		panel_2.add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("");
+		btnNewButton_6.setIcon(new ImageIcon(memorama.class.getResource("/paquete/ovni1.jpg")));
+		panel_2.add(btnNewButton_6);
+		
+		JButton btnNewButton_9 = new JButton("");
+		btnNewButton_9.setIcon(new ImageIcon(memorama.class.getResource("/paquete/persona1.jpg")));
+		panel_2.add(btnNewButton_9);
+		
+		JButton btnNewButton_8 = new JButton("");
+		btnNewButton_8.setIcon(new ImageIcon(memorama.class.getResource("/paquete/persona1.jpg")));
+		panel_2.add(btnNewButton_8);
+		
+		JButton btnNewButton_7 = new JButton("");
+		btnNewButton_7.setIcon(new ImageIcon(memorama.class.getResource("/paquete/satelite1.jpg")));
+		panel_2.add(btnNewButton_7);
+		
+		JButton btnNewButton_10 = new JButton("");
+		btnNewButton_10.setIcon(new ImageIcon(memorama.class.getResource("/paquete/satelite1.jpg")));
+		panel_2.add(btnNewButton_10);
+		
+		JButton btnNewButton_11 = new JButton("");
+		btnNewButton_11.setIcon(new ImageIcon(memorama.class.getResource("/paquete/sol1.jpg")));
+		panel_2.add(btnNewButton_11);
+		
+		JButton btnNewButton_13 = new JButton("");
+		btnNewButton_13.setIcon(new ImageIcon(memorama.class.getResource("/paquete/sol1.jpg")));
+		panel_2.add(btnNewButton_13);
+		
+		JButton btnNewButton_14 = new JButton("");
+		btnNewButton_14.setIcon(new ImageIcon(memorama.class.getResource("/paquete/tierra1.jpg")));
+		panel_2.add(btnNewButton_14);
+		
+		JButton btnNewButton_15 = new JButton("");
+		btnNewButton_15.setIcon(new ImageIcon(memorama.class.getResource("/paquete/tierra1.jpg")));
+		panel_2.add(btnNewButton_15);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(new Color(128, 255, 255));
+		panel_1.add(panel_3);
+		panel_3.setLayout(new GridLayout(3, 1, 0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		panel_3.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		panel_3.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		panel_3.add(lblNewLabel_3);
 	}
 
 }
