1b632498e2c78137d0a5d67740ec2052954a4ce6
Manuel-O-12 <joor_23@alu.uabcs.mx>
2025-04-03 03:07:24 -0700

    unidad 3 ejercicio 19

--- Letras/src/Ventana/Teclado.java
+++ Letras/src/Ventana/Teclado.java
@@ -5,14 +5,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
 public class Teclado {
 
 	private JFrame frame;
+	private JButton teclaActual;
 
 	/**
 	 * Launch the application.
@@ -46,92 +52,24 @@ public class Teclado {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
+		panel.setAutoscrolls(true);
+		panel.setBackground(new Color(192, 192, 192));
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(new GridLayout(3, 10, 0, 0));
 		
-		JButton btnNewButton = new JButton("Q");
-		panel.add(btnNewButton);
+		String[] teclas = {"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", 
+                "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ", 
+                "Z", "X", "C", "V", "B", "N", "M", "", "", ""};
 		
-		JButton btnNewButton_1 = new JButton("W");
-		panel.add(btnNewButton_1);
+		JButton[] botones = new JButton[teclas.length];
+		for (int i = 0; i < teclas.length; i++) {
+            botones[i] = new JButton(teclas[i]);
+            botones[i].setOpaque(true);
+            //botones[i].setContentAreaFilled(false);
+            panel.add(botones[i]);
+        }
 		
-		JButton btnNewButton_2 = new JButton("E");
-		panel.add(btnNewButton_2);
 		
-		JButton btnNewButton_3 = new JButton("R");
-		panel.add(btnNewButton_3);
-		
-		JButton btnNewButton_4 = new JButton("T");
-		panel.add(btnNewButton_4);
-		
-		JButton btnNewButton_5 = new JButton("Y");
-		panel.add(btnNewButton_5);
-		
-		JButton btnNewButton_6 = new JButton("U");
-		panel.add(btnNewButton_6);
-		
-		JButton btnNewButton_7 = new JButton("I");
-		panel.add(btnNewButton_7);
-		
-		JButton btnNewButton_8 = new JButton("O");
-		panel.add(btnNewButton_8);
-		
-		JButton btnNewButton_9 = new JButton("P");
-		panel.add(btnNewButton_9);
-		
-		JButton btnNewButton_10 = new JButton("A");
-		panel.add(btnNewButton_10);
-		
-		JButton btnNewButton_11 = new JButton("S");
-		panel.add(btnNewButton_11);
-		
-		JButton btnNewButton_12 = new JButton("D");
-		panel.add(btnNewButton_12);
-		
-		JButton btnNewButton_13 = new JButton("F");
-		panel.add(btnNewButton_13);
-		
-		JButton btnNewButton_14 = new JButton("G");
-		panel.add(btnNewButton_14);
-		
-		JButton btnNewButton_15 = new JButton("H");
-		panel.add(btnNewButton_15);
-		
-		JButton btnNewButton_16 = new JButton("J");
-		panel.add(btnNewButton_16);
-		
-		JButton btnNewButton_17 = new JButton("K");
-		panel.add(btnNewButton_17);
-		
-		JButton btnNewButton_18 = new JButton("L");
-		panel.add(btnNewButton_18);
-		
-		JButton btnNewButton_19 = new JButton("Ñ");
-		panel.add(btnNewButton_19);
-		
-		JButton btnNewButton_20 = new JButton("Z");
-		panel.add(btnNewButton_20);
-		
-		JButton btnNewButton_21 = new JButton("X");
-		panel.add(btnNewButton_21);
-		
-		JButton btnNewButton_22 = new JButton("C");
-		panel.add(btnNewButton_22);
-		
-		JButton btnNewButton_23 = new JButton("V");
-		panel.add(btnNewButton_23);
-		
-		JButton btnNewButton_24 = new JButton("V");
-		panel.add(btnNewButton_24);
-		
-		JButton btnNewButton_25 = new JButton("B");
-		panel.add(btnNewButton_25);
-		
-		JButton btnNewButton_26 = new JButton("N");
-		panel.add(btnNewButton_26);
-		
-		JButton btnNewButton_27 = new JButton("M");
-		panel.add(btnNewButton_27);
 		
 		JPanel panel_1 = new JPanel();
 		frame.getContentPane().add(panel_1, BorderLayout.NORTH);
@@ -148,6 +86,56 @@ public class Teclado {
 		
 		JButton btnNewButton_29 = new JButton("BORRADOR");
 		panel_2.add(btnNewButton_29);
-	}
+		
+		
+		frame.setOpacity(1);
+		frame.setFocusable(true);
+		frame.requestFocusInWindow();
+		
+		frame.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				char tecla = e.getKeyChar();
+				JButton botonActual = buscar_Boton(tecla, botones);
+                
+                if (botonActual != null) {
+                    if (teclaActual != null) {
+                        teclaActual.setBackground(null);
+                    }
+                    botonActual.setBackground(Colores());
+                    teclaActual = botonActual;
+                }
+            }
 
+            private JButton buscar_Boton(char c, JButton[] botones) {
+                for (JButton boton : botones) {
+                    if (boton.getText().equalsIgnoreCase(String.valueOf(c))) {
+                        return boton;
+                    }
+                }
+                return null;
+            }
+            
+            private Color Colores() {
+            	Random aleatorio = new Random();
+            	return new Color(aleatorio.nextInt(256), aleatorio.nextInt(256), aleatorio.nextInt(256));
+            }
+            
+        });
+		
+    }
 }
